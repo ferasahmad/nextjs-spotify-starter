0b5e482122d303b01f1ea34328249422f4db8688
Feras Ahmad <firastaf878@gmail.com>
2024-09-23 11:54:45 -0400

    changes: new fonts, styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
+import SpotifyButton from "@/components/SpotifyButton";
 import WithAuth from "@/hoc/withAuth";
+import { signOut } from "next-auth/react";
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to TopSpin</h1>
+    <div className="flex flex-1 items-center justify-center flex-col gap-4">
+      <h1 className="text-5xl">Welcome!</h1>
       <p>
         This is the protected main page. Only authenticated users can access
         this page.
       </p>
+      <SpotifyButton onClick={signOut}>Sign out</SpotifyButton>
     </div>
   );
 }
