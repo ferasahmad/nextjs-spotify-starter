95397cf110ea0f9c1d1e82428f4a36ff88d0bbf4
Feras Ahmad <firastaf878@gmail.com>
2024-11-10 14:32:26 -0500

    change: included token refresh along next-auth spotify setup

--- src/app/api/[auth]/[...nextauth]/route.ts
+++ src/app/api/[auth]/[...nextauth]/route.ts
@@ -1,19 +1,33 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
+import { type NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
+const options: NextAuthOptions = {
   providers: [
     SpotifyProvider({
+      authorization:
+        // "https://accounts.spotify.com/authorize?scope=user-top-read%20user-read-recently-played",
+        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,playlist-modify-public",
       clientId: process.env.SPOTIFY_CLIENT_ID || "",
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
-      authorization:
-        "https://accounts.spotify.com/authorize?scope=user-top-read%20user-read-recently-played",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        token,
+      };
+    },
+  },
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
