e854d99fbde02567de5f31ab3ace11542160b0ba
Feras Ahmad <firastaf878@gmail.com>
2024-11-10 21:54:38 -0500

    change: get current session and display name in private page

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import SpotifyButton from "@/components/SpotifyButton";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function HomeClient() {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className="flex flex-1 items-center justify-center flex-col gap-4">
-      <h1 className="text-5xl">Welcome!</h1>
+      <h1 className="text-5xl">Welcome {session?.user?.name}!</h1>
       <p>
         This is the protected main page. Only authenticated users can access
         this page.
