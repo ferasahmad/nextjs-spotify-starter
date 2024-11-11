f647af435298183deb56e499fd05566160fede63
Feras Ahmad <firastaf878@gmail.com>
2024-11-10 21:56:19 -0500

    remove: console.log

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -6,8 +6,6 @@ import { signOut, useSession } from "next-auth/react";
 export default function HomeClient() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return (
     <div className="flex flex-1 items-center justify-center flex-col gap-4">
       <h1 className="text-5xl">Welcome {session?.user?.name}!</h1>
